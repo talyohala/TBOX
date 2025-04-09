919148d33fd0180fd473bcc60cc387f5fbbd9154
talyohala <talyohala1@gmail.com>
2025-04-09 21:43:36 +0300

    Update Color.kt

--- app/src/main/java/com/tbox/app/ui/theme/Color.kt
+++ app/src/main/java/com/tbox/app/ui/theme/Color.kt
@@ -1,8 +1,18 @@
-package com.tbox.app.ui.theme
-
-import androidx.compose.ui.graphics.Color
-
-val Purple200 = Color(0xFFBB86FC)
-val Teal200 = Color(0xFF03DAC5)
-val Black = Color(0xFF000000)
-val White = Color(0xFFFFFFFF)
+<resources>
+    <color name="colorPrimary">#6200EE</color>
+    <color name="colorPrimaryVariant">#3700B3</color>
+    <color name="colorOnPrimary">#FFFFFF</color>
+    <color name="colorSecondary">#03DAC6</color>
+    <color name="colorSecondaryVariant">#018786</color>
+    <color name="colorOnSecondary">#000000</color>
+    <color name="colorPrimaryContainer">#BB86FC</color>
+    <color name="colorOnPrimaryContainer">#000000</color>
+    <color name="colorSecondaryContainer">#03DAC6</color>
+    <color name="colorOnSecondaryContainer">#000000</color>
+    <color name="white">#FFFFFF</color>
+    <color name="black">#000000</color>
+    <color name="purple_200">#BB86FC</color>
+    <color name="purple_700">#3700B3</color>
+    <color name="teal_200">#03DAC5</color>
+    <color name="teal_700">#018786</color>
+</resources>

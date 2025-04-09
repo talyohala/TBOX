123c5d05be50132329a0578e84213a929cdb0179
Tal-Yohala <  -GitHub>
2025-04-10 01:59:23 +0300

    ניקוי build.gradle.kts ראשי – הסרת repositories מקומי

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+// קובץ ראשי – מגדיר buildscript בלבד אם צריך
+
+// ריק זה תקין – הקובץ הזה יכול להיות גם בלי כלום אם אין צורך ב־buildscript
+
+// השורה הבאה הכרחית רק אם תשתמש ב-buildscript ברמת הפרויקט
+// buildscript {
+//     repositories {
+//         google()
+//         mavenCentral()
+//     }
+//     dependencies {
+//         // דוגמה אם תצטרך בעתיד
+//     }
+// }
